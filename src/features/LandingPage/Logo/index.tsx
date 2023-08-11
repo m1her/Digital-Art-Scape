0810c701fecc91487e2f48677747337979356b07
Maher Nassrallah <maher.ple2@gmail.com>
2023-08-11 14:45:37 +0300

    feat:loading; done

---
+++ src/features/LandingPage/Logo/index.tsx
@@ -0,0 +1,15 @@
+const Logo = () => {
+  return (
+    <div className="">
+      <div className="text-9xl font-semibold text-white tracking-widest">
+        ART
+      </div>
+      <div className="overflow-hidden -mt-4 w-[285px] ">
+        <div className="text-sm font-light text-white tracking-[60px] text-center">
+          SCAPE
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Logo;
