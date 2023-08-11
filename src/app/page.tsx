b53b63a30a1edb99a7f9e2490183eb228ffbf945
Maher Nassrallah <maher.ple2@gmail.com>
2023-08-11 15:30:36 +0300

    feat:landing-page; done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import LandingPage from "@/features/LandingPage";
+import Loading from "@/features/loading";
 import Image from "next/image";
 export default function Home() {
   return (
     <div className="fixed top-0 left-0 w-full h-screen">
-  <LandingPage />
+      <Loading />
+      <LandingPage />
     </div>
   );
 }
---
+++ src/features/LandingPage/Logo/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="">
+      <div className="text-9xl font-semibold text-white tracking-widest">
+        ART
+      </div>
+      <div className="overflow-hidden -mt-4 w-[285px] ">
+        <div className="text-sm font-light text-white tracking-[60px] text-center">
+          SCAPE
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Logo;
